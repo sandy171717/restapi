a6e8fa15563674b39de4684fcbb3dde82451cfdb
sandy171717 <maksandy405@gmail.com>
2020-09-23 14:40:26 +0530

    adding routes

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const express=require('express')
 const app=express();
 
 
+//routes
 
+app.get('/',(req,res)=>{
+
+    res.send("Hello Sandeep");
+})
+
+app.get('/add',(req,res)=>{
+    res.send('we are adding user')
+})
 
 app.listen(4000,()=>{
     console.log('server is running on port 4000')
